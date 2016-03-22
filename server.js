c7651e978035cd9128446b9ed288ad08d0ae38f8
Sebastian Udden <sebastian.udden@gmail.com>
2016-03-22 18:08:24 +0100

    Try

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
 	res.send('Hello');
 });
 
-app.listen(3000);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
+});
